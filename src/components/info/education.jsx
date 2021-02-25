9556442126f8f093532be87b670d964ce1fa3027
di-112 <73027626+di-112@users.noreply.github.com>
2021-02-25 20:09:27 +0300

    changed education

--- src/components/info/education.jsx
+++ src/components/info/education.jsx
@@ -5,9 +5,9 @@ const Education = () => {
       <section className="education">
          <h2 className="education__title title">Образование</h2>
          <EducationItem years='2016-2020' 
-         place='НИУ МИЭТ (Московский Институт Электронной Техники) 
-                МПИСУ
-                "Инфоматика и вычистлительная техника"'/>
+         university='НИУ МИЭТ'
+         faculty='МПиТК'    
+         speciality='Инфоматика и вычистлительная техника'/>
       </section>
    )
 }
@@ -16,7 +16,11 @@ const EducationItem = (props) => {
    return (
       <div className="education__item">
          <h3 className="education__years">{props.years}</h3>
-         <h3 className="education__place">{props.place}</h3>
+         <div className="education__place">{props.place}
+            <h3 className="education__university"><span>ВУЗ: </span>{props.university}</h3>
+            <h3 className="education__faculty"><span>Факультет: </span>{props.faculty}</h3>
+            <h3 className="education__speciality"><span>Направление: </span>{props.speciality}</h3>
+         </div>
       </div>
    )
 }
